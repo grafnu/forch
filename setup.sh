b8af1d41571be18187f1f11bbd9d0ff4e694e59d
Trevor Pering <peringknife@google.com>
2020-09-17 15:43:36 -0700

    Adding debug

--- setup.sh
+++
@@ -1,7 +0,0 @@
-bin/setup_stack local dhcp
-sudo ip link set t1sw1-eth9 down
-sudo ip link set t1sw2-eth10 down
-sudo ip addr add 240.0.0.1/24 dev lo
-sudo ip link set t1sw2-eth28 down 
-sudo ip link set t1sw1-eth6 down 
-sudo ip link set t1sw1-eth11 down
