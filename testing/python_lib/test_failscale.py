2b84cc800046f5ccfaa932e14a44f31249fef94e
Trevor Pering <peringknife@google.com>
2020-09-21 08:00:00 -0700

    Add debug

--- testing/python_lib/test_failscale.py
+++ testing/python_lib/test_failscale.py
@@ -21,11 +21,17 @@ class FailScaleConfigTest(IntegrationTestBase):
         """Test to build stack and check for connectivity"""
         self.assertEqual(10, self._ping_host('forch-faux-8', '192.168.1.0', count=10, output=True),
                          'warm-up ping count')
+        print('x')
         process = self._ping_host_process('forch-faux-8', '192.168.1.0', count=60)
+        print('a')
         time.sleep(5)
+        print('b')
         self._fail_egress_link()
+        print('c')
         ping_count = self._ping_host_reap(process, output=True)
+        print('d')
         self.assertTrue(ping_count > 35 and ping_count < 55, 'disrupted ping count %s' % ping_count)
+        print('e')
 
 
 if __name__ == '__main__':
